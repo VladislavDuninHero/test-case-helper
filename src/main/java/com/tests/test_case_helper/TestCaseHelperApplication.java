c816a2f5153d784276f5cf92807289510b8fd3ff
vladislavDunin <vladislavdunin.java@gmail.com>
2025-03-07 23:27:26 +0300

    init

---
+++ src/main/java/com/tests/test_case_helper/TestCaseHelperApplication.java
@@ -0,0 +1,13 @@
+package com.tests.test_case_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCaseHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tests/test_case_helper/TestCaseHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.test_case_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaseHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
