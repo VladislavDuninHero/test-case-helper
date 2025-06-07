fbec390456954317636bf50fc5f7a054043e89d5
vladislavDunin <vladislavdunin.java@gmail.com>
2025-06-07 19:57:48 +0300

    Add caching for project and test-suites, refactor app properties file to yml, fixed docker and docker compose file, refactor jar naming

--- src/main/java/com/tests/test_case_helper/TestCaseHelperApplication.java
+++ src/main/java/com/tests/test_case_helper/TestCaseHelperApplication.java
@@ -3,13 +3,14 @@ package com.tests.test_case_helper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableJpaRepositories
-@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class TestCaseHelperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestCaseHelperApplication.class, args);
